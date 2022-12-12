2a7c4cd3f076602af48baf9d714fa0e443d5f3af
Ebitari Nabiebu <ebi.nabiebu@gmail.com>
2022-12-12 02:59:56 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main : This prints a line of code using the puts() function 
+ *
+ * Returns 0
+ */
+
+int main(void){
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
