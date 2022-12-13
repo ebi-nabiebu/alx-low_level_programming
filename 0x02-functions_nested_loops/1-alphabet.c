158ff1e5f5fd6f67770478cd6e59b7feac8646ef
Ebitari Nabiebu <ebi.nabiebu@gmail.com>
2022-12-13 15:26:35 -0800

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main -A program that prints the alphabet in lowercase.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+char alpha = 'a';
+
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+
+_putchar('\n');
+return (0);
+}
+
