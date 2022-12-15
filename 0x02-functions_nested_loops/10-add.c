cda444bc9a32b53e2c5b1217fa42441f7b31ebb0
Ebitari Nabiebu <ebi.nabiebu@gmail.com>
2022-12-14 19:39:49 -0800

    Task 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return: the result
+ */
+int add(int num1, int num2)
+{
+
+int sum = num1 + num2;
+return (sum);
+}
