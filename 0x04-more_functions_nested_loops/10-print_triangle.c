20dcbc0242b2a87526da194486d4ec750f0c4c80
Ebitari Nabiebu <ebi.nabiebu@gmail.com>
2022-12-16 22:03:11 -0800

    Task 10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - Function prints a triangle, followed by a new line.
+ *
+ * @size: The Size of the Triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
