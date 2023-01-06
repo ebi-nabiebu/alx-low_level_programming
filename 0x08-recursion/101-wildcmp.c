b92bc2fe749b7b7c2ecc1f1043872b05349518e5
Ebitari Nabiebu <ebi.nabiebu@gmail.com>
2023-01-05 21:34:30 -0800

    Task 8. Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,36 +1,43 @@
 #include "main.h"
 
 /**
- * str_checker - check if two strings are identical.
- * @s1: string_1 base address.
- * @s2: string_2 base address.
- * @i: left index.
- * @j: special index. (joker)
- * Return: 1 if s is palindrome, 0 otherwise.
+ * exam_str - This function ascertains if the strings are the same.
+ *
+ * @s1: Address 1.
+ *
+ * @s2: Address 2.
+ *
+ * @eb: Index - Left.
+ *
+ * @ef: Index - Control.
+ *
+ * Return: 1 or 0.
  */
 
-int str_checker(char *s1, char *s2, int i, int j)
+int exam_str(char *s1, char *s2, int eb, int ef)
 {
-if (s1[i] == '\0' && s2[j] == '\0')
+if (s1[eb] == '\0' && s2[ef] == '\0')
 return (1);
-if (s1[i] == s2[j])
-return (str_checker(s1, s2, i + 1, j + 1));
-if (s1[i] == '\0' && s2[j] == '*')
-return (str_checker(s1, s2, i, j + 1));
-if (s2[j] == '*')
-return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+if (s1[eb] == s2[ef])
+return (exam_str(s1, s2, eb + 1, ef + 1));
+if (s1[eb] == '\0' && s2[ef] == '*')
+return (exam_str(s1, s2, eb, ef + 1));
+if (s2[ef] == '*')
+return (exam_str(s1, s2, eb + 1, ef) || exam_str(s1, s2, eb, ef + 1));
 return (0);
 }
 
 /**
- * wildcmp - check if strings could be considered identical
- * @s1: base address for string.
- * @s2: base address for string.
+ * wildcmp - This functions ascertains if the strings are identical.
  *
- * Return: 1 if are considered identical.
+ * @s1: Address 1.
+ *
+ * @s2: Address 2.
+ *
+ * Return: 1 if Strings are identical.
  */
 
 int wildcmp(char *s1, char *s2)
 {
-return (str_checker(s1, s2, 0, 0));
+return (exam_str(s1, s2, 0, 0));
 }
