dc1d49f5c298874e6407a9f42d7c5ff8eb5e9331
Ebitari Nabiebu <ebi.nabiebu@gmail.com>
2023-01-05 18:37:58 -0800

    Task 1. Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - This function prints a string in reverse.
+ *
+ * @s: The String.
+ * Return: Nothing.
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+if (*s)
+{
+	_print_rev_recursion(s + 1);
+
+	_putchar(*s);
+}
+}
