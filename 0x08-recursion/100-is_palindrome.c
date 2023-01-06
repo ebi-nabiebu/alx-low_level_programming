2b455f59dd3a5f67141dbe9900af950511a93be0
Ebitari Nabiebu <ebi.nabiebu@gmail.com>
2023-01-05 21:20:11 -0800

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,40 +1,51 @@
 #include "main.h"
+
 /**
- * _strlen_recursion - Prints the length of a string.
- * @s: the string to be printed
- * Return: the length of string
+ * _strlen_recursion - This function prints the length of the String.
+ *
+ * @s: String
+ *
+ * Return: The Length of The String.
  */
+
 int _strlen_recursion(char *s)
 {
 if (s[0] != '\0')
 return (1 + _strlen_recursion(s + 1));
 return (0);
 }
+
 /**
- * pal_checker - check if s is palindrome.
- * @s: string base address.
- * @i: left index.
- * @j: right index.
- * Return: 1 if s is palindrome, 0 otherwise.
+ * exam - This function ascertains if s is a palindrome.
+ * @s: Address
+ *
+ * @eb: Index - Left.
+ *
+ * @eg: Index - Right.
+ *
+ * Return: 1 or 0 if s is a palindrome or otherwise respectively.
  */
 
-int pal_checker(char *s, int i, int j)
+int exam(char *s, int eb, int eg)
 {
-if (s[i] == s[j])
-if (i > j / 2)
+if (s[eb] == s[eg])
+if (eb > eg / 2)
 return (1);
 else
-return (pal_checker(s, i + 1, j - 1));
+return (exam(s, eb + 1, eg - 1));
 else
 return (0);
 }
+
 /**
- * is_palindrome - check if s is palindrome
- * @s: base address for string.
+ * is_palindrome - This function returns 1 if a string is a palindrome, or 0.
  *
- * Return: 1 if n is prime, 0 otherwise.
+ * @s: Address
+ *
+ * Return: 1 or 0 if n is prime or otherwise respectively.
  */
+
 int is_palindrome(char *s)
 {
-return (pal_checker(s, 0, _strlen_recursion(s) - 1));
+return (exam(s, 0, _strlen_recursion(s) - 1));
 }
